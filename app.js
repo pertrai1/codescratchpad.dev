4f6b9b03b591ceb08483f9a0ae08a496c31b83ad
Rob Simpson <rsimpson2@me.com>
2023-05-24 16:54:13 -0400

    add websocket

--- app.js
+++ app.js
@@ -10,3 +10,28 @@ server.on("request", app);
 server.listen(3000, function() {
 	console.log("Server started on port 3000");
 });
+
+// being websockets
+const WebSocketServer = require("ws").Server;
+const wss = new WebSocketServer({server});
+
+wss.on("connection", function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log(`Clients connected ${numClients}`);
+  
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("Welcome to my server");
+  }
+
+  ws.on("close", function close() {
+    console.log("the client has disconnected");
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
