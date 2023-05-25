c60d200f68f1e1abf3c1031a205cda0f8649e587
Rob Simpson <rsimpson2@me.com>
2023-05-25 09:32:15 -0400

    add sqlite3

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get("/", function(req, res) {
 });
 
 server.on("request", app);
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
 server.listen(3000, function() {
 	console.log("Server started on port 3000");
 });
@@ -25,6 +33,10 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to my server");
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+  `);
+
   ws.on("close", function close() {
     console.log("the client has disconnected");
   });
@@ -35,3 +47,48 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+// END WEBDOCKET
+
+// BEGIN DATABASE
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("Shutting down db");
+  db.close();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
